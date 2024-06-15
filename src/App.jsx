7c0ff575cb9e9804c5534de8e7f24837ab1bb63e
Kashish Gupta <guptakashishiit40@gmail.com>
2024-06-15 17:03:43 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kashish Gupta",
+  title: "Web Developer",
+  email: "guptakashishiit40@gmail.com",
+  gitHub: "https://github.com/kashishgupta40",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/kashish-gupta-064575260/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
